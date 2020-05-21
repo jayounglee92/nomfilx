4daef85fbc4020bc6c802962066954d872cd8449
Jayoung Lee <39401563+jayounglee92@users.noreply.github.com>
2020-05-21 18:34:38 +0900

    GlobalStyles and Header

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -3,15 +3,18 @@ import {BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom
 import Home from 'Routes/Home';
 import TV from 'Routes/TV';
 import Search from 'Routes/Search';
-import Detail from 'Routes/Detail';
+import Header from 'Components/Header';
 
 export default () => (
     <Router>
+        <>
+        <Header/>
         <Switch>
             <Route path="/" exact component={Home}/>
             <Route path="/tv" component={TV}/>
             <Route path="/search" component={Search}/>
             <Redirect from="*" to="/"/>
         </Switch> 
+        </>
     </Router>
 )
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from 'Components/App';
+import "style.css";
+
 
 ReactDOM.render(
   <React.StrictMode>
